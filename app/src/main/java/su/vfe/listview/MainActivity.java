48fe92cd8c1c56e120ce580319e7ad08d0fca39a
vershinin-fe <few_w@mail.ru>
2018-07-15 13:38:15 +0500

    Add layout for ListView rows Add custom ArrayAdapter

--- app/src/main/java/su/vfe/listview/MainActivity.java
+++ app/src/main/java/su/vfe/listview/MainActivity.java
@@ -1,9 +1,17 @@
 package su.vfe.listview;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +28,35 @@ public class MainActivity extends AppCompatActivity {
                 "Once", "Doce", "Trece"
         };
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numeros);
+        ArrayAdapter<String> adapter = new CustomArrayAdapter(this, numeros);
 
         listView.setAdapter(adapter);
+    }
+
+    private static class CustomArrayAdapter extends ArrayAdapter<String> {
+        private final Context context;
+        private final String[] values;
+
+        public CustomArrayAdapter(Context context, String[] values) {
+            super(context, R.layout.row_layout, values);
+            this.context = context;
+            this.values = values;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.row_layout, parent, false);
+
+            TextView textView = (TextView) rowView.findViewById(R.id.text);
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.preview);
+
+            textView.setText(values[position]);
+            imageView.setImageResource(R.drawable.ic_launcher_background);
+
+            return rowView;
+        }
     }
 }
