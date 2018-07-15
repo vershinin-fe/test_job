2c80b9a2cda1416715cfd2077c9663301f84feea
vershinin-fe <few_w@mail.ru>
2018-07-15 10:56:45 +0500

    Add listView with fake data Remove ConstraintLayout dependency

--- app/src/main/java/su/vfe/listview/MainActivity.java
+++ app/src/main/java/su/vfe/listview/MainActivity.java
@@ -2,6 +2,8 @@ package su.vfe.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        final String[] numeros = new String[] {
+                "Uno", "Dos", "Tres", "Cuatro", "Cinco",
+                "Seis", "Siete", "Ocho", "Nueve", "Diez",
+                "Once", "Doce", "Trece"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numeros);
+
+        listView.setAdapter(adapter);
     }
 }
